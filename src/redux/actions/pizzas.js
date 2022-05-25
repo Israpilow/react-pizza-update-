6abd4085d058087a4ebe38184bc86273133943b1
Jamal01~ <ubayd200@gmail.com>
2022-05-25 17:29:37 +0300

    add mockApi

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -9,7 +9,7 @@ export const fetPizzas = (category, sortBy, pagination, search) => (dispatch) =>
   dispatch(setLoaded(false));
   axios
     .get(
-      `https://israpilow.github.io/react-pizza-update-/pizzas?${search ? `q=${search}` : ''}${
+      `https://62877047e9494df61b38ebfa.mockapi.io/pizzas?${search ? `q=${search}` : ''}${
         category !== null ? `category=${category}` : ''
       }&_page=${pagination.page}&_limit=${pagination.limit}&_sort=${sortBy.type}${
         sortBy.order !== undefined ? `&_order=${sortBy.order}` : '&_order=desc'
