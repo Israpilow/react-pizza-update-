667d5eb9c9da480e46650d4a9243d031e2f8e740
Jamal01~ <ubayd200@gmail.com>
2022-05-26 10:34:01 +0300

    add mockApi

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -9,10 +9,10 @@ export const fetPizzas = (category, sortBy, pagination, search) => (dispatch) =>
   dispatch(setLoaded(false));
   axios
     .get(
-      `https://62877047e9494df61b38ebfa.mockapi.io/pizzas?${search ? `q=${search}` : ''}${
+      `https://62877047e9494df61b38ebfa.mockapi.io/pizzas?${search ? `search=${search}` : ''}${
         category !== null ? `category=${category}` : ''
-      }&_page=${pagination.page}&_limit=${pagination.limit}&_sort=${sortBy.type}${
-        sortBy.order !== undefined ? `&_order=${sortBy.order}` : '&_order=desc'
+      }&page=${pagination.page}&limit=${pagination.limit}&sortBy=${sortBy.type}${
+        sortBy.order !== undefined ? `&order=${sortBy.order}` : '&order=desc'
       }`,
     )
     .then(({ data }) => {
